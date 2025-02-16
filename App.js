16f03e7160eee17fc76cf50485664c9cac0c3b67
hukadarn <jeromejauneau@outlook.fr>
2025-02-16 09:08:39 +0100

    envoi app

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+//import Logo from './components/logo
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#0c2461",
+  },
+});
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+}
